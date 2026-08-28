e58c4d6768f33668ac937e8a102c0d7ea53a7511
wwenyu06-art <wwenyu06@gmail.com>
2026-08-28 17:41:54 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  output: "export", // ✅开启静态导出，生成out文件夹
+  images: {
+    unoptimized: true // ✅关闭图片优化，pages不支持
+  }
+};
 
 export default nextConfig;
